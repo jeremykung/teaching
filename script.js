a2c4f43ebe38f62eb2ce129af86368d3de43b308
mrkung <jkung@pacificamerican.org>
2020-10-22 14:23:15 +0800

    finalized index page

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const enterButton = () => {
-  location.href = 'main.html'
+  setTimeout(function() {location.href = 'main.html'}, 2000)
+  document.querySelector('.enter-container').style.opacity = 0;
+  // location.href = 'main.html'
 }
+
+// setTimeout(function(){ alert("Hello"); }, 3000);
